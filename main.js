3e59e211288953dae70c6f19286d74a1e5a06a5e
PentaTrax <philipnordnielsen@gmail.com>
2021-11-03 16:04:56 +0100

    more website stuff

--- main.js
+++ main.js
@@ -8,14 +8,17 @@ const expressLaytouts = require('express-ejs-layouts');
 const app = express();
 
 const indexRouter = require('./routes/index');
+const communityRouter = require('./routes/community');
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/layout');
+app.set('views', __dirname + "/views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLaytouts);
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+app.use('/community', communityRouter);
 
 app.listen(process.env.PORT || 3000);
