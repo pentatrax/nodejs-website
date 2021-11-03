0bdd03d11b6398d773a05738a68aefd5f1225ddb
PentaTrax <philipnordnielsen@gmail.com>
2021-11-03 12:56:07 +0100

    Initial Setup

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const { urlencoded } = require('body-parser');
 const express = require('express');
 const expressLaytouts = require('express-ejs-layouts');
