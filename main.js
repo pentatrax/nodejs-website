1c147a69faa79e72b8f3d1b09ba23b5480b5c4fd
PentaTrax <philipnordnielsen@gmail.com>
2021-11-03 17:30:41 +0100

    file types

--- main.js
+++ main.js
@@ -16,7 +16,13 @@ app.set('views', __dirname + "/views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLaytouts);
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public", {
+    index: false, 
+    immutable: true, 
+    cacheControl: true,
+    maxAge: "30d"
+}));
+
 
 app.use('/', indexRouter);
 app.use('/community', communityRouter);
