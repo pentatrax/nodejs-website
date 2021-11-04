e5ccbc56625f88ec19402ce0c2e785d6fe85478b
PentaTrax <philipnordnielsen@gmail.com>
2021-11-04 17:51:26 +0100

    pages

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Index' });
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' });
+});
 
 module.exports = router;
